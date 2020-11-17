626e95d8cfe106ad7a0449ea3bf29dd43740a961
devozzi <oguzhan.rock@hotmail.com>
2020-11-17 19:26:06 +0300

    uploadActivity/ refactor

--- app/src/main/java/com/devozz/evodeme/UploadActivity.kt
+++ app/src/main/java/com/devozz/evodeme/UploadActivity.kt
@@ -56,7 +56,7 @@ class UploadActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray
     ) {
         if (requestCode == 1) {
-            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 startActivityForResult(intent,2)
             }
